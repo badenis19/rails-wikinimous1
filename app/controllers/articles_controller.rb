7650be7cd0195811ccc9dbdf3b46cde7449d161e
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-02-29 17:47:20 +0000

    link to edit page + before action to be DRY

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -18,16 +19,14 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     @article.update(article_params)
+    redirect_to articles_path
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
 
     redirect_to articles_path
@@ -39,5 +38,8 @@ class ArticlesController < ApplicationController
     params.require(:article).permit(:title, :content);
   end
 
+  def find_article 
+    @article = Article.find(params[:id])
+  end
 
 end
